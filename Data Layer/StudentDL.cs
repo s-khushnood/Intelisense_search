80ff65d1db00d40dfc96f922bb3221f9c7bf61a0
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-08-03 06:47:44 -0400

    Add files via upload

---
+++ Data Layer/StudentDL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using StudentIntelisenceApi.Models;
+
+namespace StudentIntelisenceApi.Data_Layer
+{
+    public class StudentDL
+    {
+        string _connectionString = "";
+        public StudentDL()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings["DemoCN"].ConnectionString;
+        }
+        public DataTable GetAllStudents()
+        {
+            try
+            {
+                List<Student> listStudents = new List<Student>();
+                DataTable dataTable = new DataTable();
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    SqlCommand command = new SqlCommand("FetchAllStudents", con);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    con.Open();
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    con.Close();
+                }
+                return dataTable;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("An exception of type " + exception.GetType().ToString()
+                   + " is encountered in GetListOfStudents due to "
+                   + exception.Message, exception.InnerException);
+            }
+        }
+        public DataTable GetbyName(string str)
+        {
+            try
+            {
+                List<Student> listStudents = new List<Student>();
+                DataTable dataTable = new DataTable();
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    SqlCommand command = new SqlCommand("FetchbyName", con);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@name", str);
+                    con.Open();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    con.Close();
+                }
+                return dataTable;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("An exception of type " + exception.GetType().ToString()
+                   + " is encountered in GetListOfStudents due to "
+                   + exception.Message, exception.InnerException);
+            }
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentIntelisenceApi.Models
+{
+    public class Student
+    {
+       public int ID { get; set; }
+       public  string FirstName { get; set; }
+       public string LastName { get; set; }
+       public string Course { get; set; }
+       public int Age { get; set; }
+    }
+}
