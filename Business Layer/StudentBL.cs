3c01df4bef4a477c6107efda42ffecd5641f896c
s khushnood <138812257+s-khushnood@users.noreply.github.com>
2023-08-03 06:47:13 -0400

    Add files via upload

---
+++ Business Layer/StudentBL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using StudentIntelisenceApi.Models;
+using StudentIntelisenceApi.Data_Layer;
+
+namespace StudentIntelisenceApi.Business_Layer
+{
+    public class StudentBL
+    {
+        public StudentDL dataLayer = new StudentDL();
+        public List<Student> GetAllStudents()
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                List<Student> listStudents = new List<Student>();
+                table = dataLayer.GetAllStudents();
+
+                if (table != null && table.Rows.Count > 0)
+                {
+                    foreach (DataRow dataRow in table.Rows)
+                    {
+                        Student student = new Student();
+                        student.ID = Convert.ToInt32(dataRow["ID"]);
+                        student.FirstName = dataRow["FirstName"].ToString();
+                        student.LastName = dataRow["LastName"].ToString();
+                        student.Course = dataRow["Course"].ToString();
+                        student.Age = Convert.ToInt32(dataRow["Age"].ToString());
+                        listStudents.Add(student);
+                    }
+                }
+                return listStudents;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("An exception of type " + exception.GetType().ToString()
+                   + " is encountered in GetListOfStudents due to "
+                   + exception.Message, exception.InnerException);
+            }
+        }
+        public List<Student> GetbyName(string str)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                List<Student> listStudents = new List<Student>();
+                table = dataLayer.GetbyName(str);
+
+                if (table != null && table.Rows.Count > 0)
+                {
+                    foreach (DataRow dataRow in table.Rows)
+                    {
+                        Student student = new Student();
+                        student.ID = Convert.ToInt32(dataRow["ID"]);
+                        student.FirstName = dataRow["FirstName"].ToString();
+                        student.LastName = dataRow["LastName"].ToString();
+                        student.Course = dataRow["Course"].ToString();
+                        student.Age = Convert.ToInt32(dataRow["Age"].ToString());
+                        listStudents.Add(student);
+                    }
+                }
+                return listStudents;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("An exception of type " + exception.GetType().ToString()
+                   + " is encountered in GetListOfStudents due to "
+                   + exception.Message, exception.InnerException);
+            }
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentIntelisenceApi.Business_Layer;
+using StudentIntelisenceApi.Models;
+namespace StudentIntelisenceApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public StudentBL datalayer=new StudentBL();
+        [HttpGet]
+        [Route("GetAllStudents")]
+        public List<Student> GetAllStudents()
+        {
+            try
+            
+            {
+                List<Student> students = new List<Student>();
+                students = datalayer.GetAllStudents();
+                return students;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("An exception of type " + exception.GetType().ToString()
+                   + " is encountered in GetListOfStudents due to "
+                   + exception.Message, exception.InnerException);
+            }
+        }
+
+        [HttpGet]
+        [Route("FetchbyName")]
+        public List<Student> GetbyName(string str)
+        {
+            if (str!=null)
+            {
+                try
+
+                {
+                    List<Student> students = new List<Student>();
+                    students = datalayer.GetbyName(str);
+                    return students;
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception("An exception of type " + exception.GetType().ToString()
+                       + " is encountered in GetListOfStudents due to "
+                       + exception.Message, exception.InnerException);
+                }
+            }
+            else
+            {
+                List<Student> emptystudent = new List<Student>();
+                return emptystudent;
+            }
+        }
+    }
+}
